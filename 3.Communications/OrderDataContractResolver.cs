d3905862096103ca4000a3236c7c6e3d8802ea09
VokiLook <vokilook@users.noreply.github.com>
2019-12-11 15:49:24 +0300

    Order resolver for "user_open_order" implementation
    Answer of "user_open_order" includes param "created" instead of "date".

---
+++ 3.Communications/OrderDataContractResolver.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Reflection;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using ExmoApiX.AuthenticatedAPIRequests;
+
+namespace ExmoApiX.Communications
+{
+    /// <summary>
+    /// Order resolver realization for "user_open_order" function.
+    /// </summary>
+    /// <remarks> Answer of "user_open_order" includes param "created" instead of "date".</remarks>
+    class OrderDataContractResolver : DefaultContractResolver
+    {
+        public static readonly OrderDataContractResolver Instance = new OrderDataContractResolver();
+
+        protected override JsonProperty CreateProperty(MemberInfo member, MemberSerialization memberSerialization)
+        {
+            var property = base.CreateProperty(member, memberSerialization);
+            if (property.DeclaringType == typeof(Order))
+            {
+                if (property.PropertyName.Equals("date", StringComparison.OrdinalIgnoreCase))
+                {
+                    property.PropertyName = "created";
+                }
+            }
+            return property;
+        }
+    }
+}
